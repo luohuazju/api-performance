50a12df265aa3ac1a6b537f101530b50e386a707
Hua Luo <luohuazju@gmail.com>
2022-01-14 16:51:51 -0600

    init the project

---
+++ src/test/scala/com/sillycat/performance/base/PerformanceTesterEnvironment.scala
@@ -0,0 +1,26 @@
+package com.sillycat.performance.base
+
+import com.typesafe.config.ConfigFactory
+
+trait PerformanceTesterEnvironment {
+
+  val config = ConfigFactory.load
+
+  val env = "env." + config.getString("build.env")
+
+  private def path(s: String*) = s.mkString(".")
+
+  def envStr(s: String): String = {
+    path(env, s)
+  }
+
+  val rampSeconds = 1
+
+  val rampNumOfUsers = config.getInt(envStr("ramp.user.num"))
+
+  val requestsPerUser = config.getInt(envStr("requests.per.user"))
+
+  val classifierHost = config.getString(envStr("classifier.host"))
+
+
+}
